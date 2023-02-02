91c9179866879e7a1d924d7669163b769078f5e3
Kholiq <kholiqcode@gmail.com>
2023-02-03 01:44:05 +0700

    refactor: setup babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: [
+          {'@components': './src/components'},
+          {'@navigation': './src/navigation'},
+          {'@screens': './src/screens'},
+        ],
+      },
+    ],
+  ],
 };
