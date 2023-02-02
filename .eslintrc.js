8deb98ffceff78f7dc29f551c8aad542dce9f463
Kholiq <kholiqcode@gmail.com>
2023-02-03 01:17:32 +0700

    refactor: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,67 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  plugins: ['import', 'prettier', 'simple-import-sort'],
+  extends: [
+    'airbnb-typescript/base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'no-unused-vars': 2,
+    'no-param-reassign': 2,
+    'no-restricted-imports': 2,
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    // simple-import-sort
+    'sort-imports': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // 1. Side effect imports
+          ['^\\u0000'],
+          // 2. React and React Native imports
+          ['^react$', '^react-native$', '^react/', '^react-native/'],
+          // 3. Any other 3rd party imports
+          ['^@?\\w'],
+          // 4. Our modules, other than the module the current file is part of
+          [
+            '^@components?\\w',
+            '^@screens?\\w',
+          ],
+          // 5. Other parts of the same module that the current file is part of
+          ['^\\.'],
+          // 6. Styles
+          ['^\\./styles'],
+          // 7. Types
+          ['^.*\\u0000$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+  },
 };
