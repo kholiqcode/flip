8f43a7c86d534815beaed607eee49d8239206659
Kholiq <kholiqcode@gmail.com>
2023-02-04 04:15:28 +0700

    refactor: add hooks to path alias

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         alias: [
           {'@constants': './src/constants'},
           {'@assets': './src/assets'},
+          {'@hooks': './src/hooks'},
           {'@store': './src/store'},
           {'@services': './src/services'},
           {'@components': './src/components'},
