4f760d734205f4d538952c9f6a4ce607dccab6c0
Kholiq <kholiqcode@gmail.com>
2023-02-03 19:46:12 +0700

    feat: add icon arrow right

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,6 @@
+import ICArrowRight from '@assets/icons/ICArrowRight';
 import ICChevronDown from '@assets/icons/ICChevronDown';
 import ICCopy from '@assets/icons/ICCopy';
 import ICSearch from '@assets/icons/ICSearch';
 
-export {ICChevronDown, ICCopy, ICSearch};
+export {ICArrowRight, ICChevronDown, ICCopy, ICSearch};
