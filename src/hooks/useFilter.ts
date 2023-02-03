a91cd81537617a17f234601062cc4be4edbacc50
Kholiq <kholiqcode@gmail.com>
2023-02-04 04:09:28 +0700

    fix: upda memoization useFilter

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,4 +1,4 @@
-import {useCallback, useMemo, useState} from 'react';
+import {useMemo, useState} from 'react';
 
 export type Filter<T> = {
   search: {
@@ -18,40 +18,36 @@ export default function useFilter<T>(
 ): T[] {
   const [searchedData, setSearchedData] = useState(data);
 
-  const filterData = useCallback(() => {
-    let searched: T[] = data;
-    if (search.query) {
-      searched = data.filter(item =>
-        search.field.some(
-          key =>
-            key &&
-            item[key] &&
-            item[key]
-              .toString()
-              .toLowerCase()
-              .includes(search.query.toLowerCase()),
-        ),
-      );
-    }
-    if (sort.field) {
-      searched = searched.sort((a, b) => {
-        if (a[sort.field] < b[sort.field]) {
-          return sort.order === 'asc' ? -1 : 1;
-        }
-        if (a[sort.field] > b[sort.field]) {
-          return sort.order === 'asc' ? 1 : -1;
-        }
-        return 0;
-      });
-    }
-    setSearchedData(searched);
-  }, [search, sort]);
-
   useMemo(() => {
     if (data.length) {
-      filterData();
+      let searched: T[] = [...data];
+      if (search.query) {
+        searched = data.filter(item =>
+          search.field.some(
+            key =>
+              key &&
+              item[key] &&
+              item[key]
+                .toString()
+                .toLowerCase()
+                .includes(search.query.toLowerCase()),
+          ),
+        );
+      }
+      if (sort.field) {
+        searched = searched.sort((a, b) => {
+          if (a[sort.field] < b[sort.field]) {
+            return sort.order === 'asc' ? -1 : 1;
+          }
+          if (a[sort.field] > b[sort.field]) {
+            return sort.order === 'asc' ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      setSearchedData(searched);
     }
-  }, [data]);
+  }, [search.query, sort, data]);
 
   return searchedData;
 }
