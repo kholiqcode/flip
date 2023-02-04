832f14563330971d66a1841d831093db5dd7b4eb
Kholiq <kholiqcode@gmail.com>
2023-02-04 08:07:54 +0700

    fix: wrong date format

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -5,8 +5,13 @@ export function formatCurrency(amount: number): string {
 export function formatDate(date: string): string {
   const dateFormat = new Date(date);
   const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
     month: 'long',
+    year: 'numeric',
   };
   const formattedMonth = dateFormat.toLocaleDateString('id-ID', options);
-  return `${dateFormat.getDay()} ${formattedMonth} ${dateFormat.getFullYear()}`;
+
+  const splitDate = formattedMonth.split(' ');
+
+  return `${splitDate[0]} ${splitDate[1]} ${splitDate[2]}`;
 }
